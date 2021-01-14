3057be73740b8a242a60686ac236d0ccba1f8c4f
David McKenna <dmckenna@cp.dias.ie>
2021-01-14 21:39:33 +0000

    Apparently MAP_POPULATE reads until you run out of memory. No thanks. That's the opposite of what I'm trying to achieve at the moment.

--- src/lib/lofar_udp_reader.c
+++ src/lib/lofar_udp_reader.c
@@ -444,7 +444,7 @@ lofar_udp_reader* lofar_udp_file_reader_setup(FILE **inputFiles, lofar_udp_meta 
 			reader.dstream[port] = ZSTD_createDStream();
 			ZSTD_initDStream(reader.dstream[port]);
 
-			tmpPtr = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE | MAP_POPULATE, tmpFd, 0);
+			tmpPtr = mmap(NULL, fileSize, PROT_READ, MAP_PRIVATE, tmpFd, 0);
 
 			// Setup the compressed data buffer/struct
 			reader.readingTracker[port].size = fileSize;
