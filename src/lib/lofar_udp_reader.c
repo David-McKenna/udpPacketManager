4654d85d67681f7339f368ab0eaf8bc252e51fdb
David McKenna <dmckenna@cp.dias.ie>
2021-02-01 13:04:14 +0000

    More compile fixes

--- src/lib/lofar_udp_reader.c
+++ src/lib/lofar_udp_reader.c
@@ -1673,7 +1673,7 @@ int lofar_udp_get_first_packet_alignment_meta(lofar_udp_reader *reader) {
 
 	// If ports are severely out of sync, read until we find the lowst common packet
 	for (int port = 0; port < meta->numPorts; port++) {
-		while (portStartingPacket[port] =< maxPacketNumber) {
+		while (portStartingPacket[port] <= maxPacketNumber) {
 			fprintf(stderr, "WARNING: Port %d is significantly below the target packet (%ld vs %ld), reading new block to syncronise the data.\n", port, portStartingPacket[port], maxPacketNumber);
 			nchars = lofar_udp_reader_nchars(reader, port, meta->inputData[port], meta->packetsPerIteration * meta->portPacketLength[port], 0);
 			portStartingPacket[port] = lofar_get_packet_number(&(meta->inputData[port][(meta->packetsPerIteration - 1) * meta->portPacketLength[port]]));
