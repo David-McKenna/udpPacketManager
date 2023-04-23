ed8f976c386a739b1df37cafe46c61c998dbf2bb
David McKenna <dmckenna@cp.dias.ie>
2023-04-23 16:20:11 +0100

    CI improvements (known failures in old GCC versions)

--- build.sh
+++ build.sh
@@ -2,12 +2,11 @@ rm -rf ./build
 mkdir build
 cd build
 
-if [[ $1 -eq 1 ]]
-then
-	cmake -DCMAKE_BUILD_TYPE=Debug ..
-	cmake --build . --target all --config Debug -- VERBOSE=1 $BUILD_FLAGS
-else
-	cmake -DCMAKE_BUILD_TYPE=Release ..
-	cmake --build . --target all --config Release
-fi
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --target all --config Release
+
 cmake --install .
+
+if [[ $1 -eq 1 ]]; then
+  ctest -V .
+fi
