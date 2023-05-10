e5de758acff30b7ae8a29482c3fa64bf8e25f6d7
David McKenna <dmckenna@cp.dias.ie>
2023-05-10 14:33:37 +0100

    Consolidate main.yml CI jobs (matrix -> runtime ENV)
    Previous matrix method resulted in the last 5 jobs hanging indefinitely,
    "Waiting for runners".

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ rm -rf ./build
 mkdir build
 cd build
 
+echo "Preparing build with CC=${CC} CXX=${CXX} LD=${LD}"
 cmake -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . --target all --config Release
 
