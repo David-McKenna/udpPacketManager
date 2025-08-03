00ee02f868687051969df25f2107d21ce97bd710
David McKenna <mckenna@astron.nl>
2025-08-03 13:28:01 +0200

    TIDY: split install/test out as well

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ set -e
 
 bash ./build_prep.sh "${1}"
 bash ./build_compile.sh "${1}"
+bash ./build_install.sh "${1}"
--- build_compile.sh
+++ build_compile.sh
@@ -7,16 +7,3 @@ if [[ $1 -eq 2 ]]; then
 fi
 
 cmake --build . --target all --config Release
-
-cmake --install .
-exitCode="$?"
-
-if [[ $1 -gt 0 ]]; then
-  ctest -V .
-  exitTest="$?"
-  if [[ $exitCode -eq 0 ]]; then
-    exitCode="${exitTest}"
-  fi
-fi
-
-exit ${exitCode}
